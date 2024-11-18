d0aac478b95364d9ac304d78a172b8dc12c95181
David Osorio <davidoso7901@gmail.com>
2024-11-18 07:01:19 -0500

    mandar

--- src/lib/server/database/connect.ts
+++ src/lib/server/database/connect.ts
@@ -1,10 +1,10 @@
-import { TURSO_CONNECTION_URL, TURSO_AUTH_TOKEN } from '$env/static/private'
+import "dotenv/config"
 import { drizzle } from 'drizzle-orm/libsql';
 import { createClient } from '@libsql/client';
 import * as schema from './data';
 
 const client = createClient({
-  url: TURSO_CONNECTION_URL,
-  authToken: TURSO_AUTH_TOKEN,
+  url:  process.env.TURSO_CONNECTION_URL!,
+  authToken: process.env.TURSO_AUTH_TOKEN,
 });
 export const db = drizzle(client, { schema });
